3dd84c21feacf17e5b3f2cb7b7ca023b36203f40
patrick breier <pbreier@mpi-cbg.de>
2022-01-05 09:53:51 +0100

    working

--- max_proj.py
+++ max_proj.py
@@ -15,10 +15,10 @@ file_number = sys.argv[3]
 #img_name = f"{input_dir}{file_name}{file_number}.tif"
 # use regex for file input
 regex = re.compile(f".+({file_number})")  # finds all files that end on the input number
-for files in os.walk(input_dir):
+for root, dirs, files in os.walk(input_dir):
     for file in files:
-        if regex.match(files):
-            img_name = f"{input_dir}{files}"  # will this stay in the loop or can I use it?
+        if regex.match(file):
+            img_name = f"{input_dir}{file}"  # will this stay in the loop or can I use it?
             file_name = f"{file}".rstrip('_N2V.tif')
 
 img = io.imread(img_name)
@@ -26,9 +26,9 @@ img_max = np.max(img, axis=-3)  # figure out if that really does what i want! do
 
 # use tiff writer to get proper hyperstack
 if len(img_max.shape) == 3:
-    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}{file_number}.tif", dim_order="TYX")
+    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}.tif", dim_order="TYX")
 elif len(img_max.shape) == 2:
-    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}{file_number}.tif", dim_order="YX")
+    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}.tif", dim_order="YX")
 else:
     print("image dimensions are not as expected")
     exit(1)
