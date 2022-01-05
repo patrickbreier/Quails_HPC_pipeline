28a0a7e148b1350d1e577bb8ec3660ee294a5248
patrick breier <pbreier@mpi-cbg.de>
2022-01-05 10:44:34 +0100

    working

--- max_proj.py
+++ max_proj.py
@@ -4,23 +4,30 @@ import sys
 import numpy as np
 from skimage import io
 from aicsimageio.writers import OmeTiffWriter
+import re
+import os
 
 # get arguments from bash script
 input_dir = sys.argv[1]
 save_dir = sys.argv[2]
 file_number = sys.argv[3]
-file_name = sys.argv[4]
 
-img_name = f"{input_dir}{file_name}{file_number}.tif"
+# use regex for file input
+regex = re.compile(f".+({file_number})")  # finds all files that end on the input number
+for root, dirs, files in os.walk(input_dir):
+    for file in files:
+        if regex.match(file):
+            img_name = f"{input_dir}{file}"  # will this stay in the loop or can I use it?
+            file_name = f"{file}"
+
 img = io.imread(img_name)
 img_max = np.max(img, axis=-3)  # figure out if that really does what i want! does it work with timelapse? use negative index to make it work for 3D data as well
 
-
 # use tiff writer to get proper hyperstack
 if len(img_max.shape) == 3:
-    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}{file_number}.tif", dim_order="TYX")
+    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}", dim_order="TYX")
 elif len(img_max.shape) == 2:
-    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}{file_number}.tif", dim_order="YX")
+    OmeTiffWriter.save(img_max, f"{save_dir}max_{file_name}", dim_order="YX")
 else:
     print("image dimensions are not as expected")
     exit(1)
--- start_max_template.sh
+++ start_max_template.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem-per-cpu=60000
 #SBATCH --mail-user=pbreier@mpi-cbg.de
 #SBATCH --mail-type=None
-#SBATCH --output=/projects/Quails/scripts/output/slurm-projection_%j.out
-#SBATCH --error=/projects/Quails/scripts/errors/slurm-projection_%j.err
+#SBATCH --output=/projects/Quails/scripts/output/slurm-projection_%A_%a.out
+#SBATCH --error=/projects/Quails/scripts/errors/slurm-projection_%%A_%a.err
 #SBATCH --array=1-==number_of_tiles==
 
 
@@ -21,4 +21,4 @@ eval "$(conda shell.bash hook)"
 conda activate quail
 
 # start projection
-/projects/Quails/scripts/pipeline/max_proj.py "${input_dir}" "${output_dir}" $(printf "%02d" "$SLURM_ARRAY_TASK_ID") "${filename}"
+/projects/Quails/scripts/pipeline/max_proj.py "${input_dir}" "${output_dir}" $(printf "%02d" "$SLURM_ARRAY_TASK_ID")
